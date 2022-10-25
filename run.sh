af6be130377cf13826084841efa8d1aeb364ab92
arkady <arksap2002@gmail.com>
2022-10-25 21:21:21 +0200

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-readarray -t filenames < repositories.txt
+readarray -t filenames <repositories.txt
 
-for filename in ${filenames[@]};
-do
-  git clone "https://github.com/$filename"
+index=1
+
+for filename in ${filenames[@]}; do
+  echo "Progress: $index/100"
+  ((index++))
+  git clone "https://github.com/$filename" 2>file.log
   ./processPhpFiles.sh
-  rm -rf "${filename#*/}"
+  rm -rf "${filename#*/}" 2>file.log
 
 done
